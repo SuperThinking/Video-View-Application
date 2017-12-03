6c7eeea4ebe5e2a708c7625ed1e5fda0011e2e05
Vishal Dhawan <superdhawan@gmail.com>
2017-12-03 12:19:24 +0530

    Playing Youtube Video Only

---
+++ app/src/main/java/straightforwardapps/dectwo/Config.java
@@ -0,0 +1,10 @@
+package straightforwardapps.dectwo;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAdSdCtIqIGIm2OwZ3wwqYiaBZKAIoTnXQ";
+
+}
--- app/src/main/java/straightforwardapps/dectwo/MainActivity.java
+++ app/src/main/java/straightforwardapps/dectwo/MainActivity.java
@@ -1,6 +1,7 @@
 package straightforwardapps.dectwo;
 
-import android.content.Intent;
+
+/*import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,15 @@ import android.view.View;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import javax.xml.datatype.Duration;
 
@@ -45,3 +55,94 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+*/
+
+    import com.google.android.youtube.player.YouTubeBaseActivity;
+    import android.content.Intent;
+    import android.net.Uri;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.Toast;
+
+    import com.google.android.youtube.player.YouTubeBaseActivity;
+    import com.google.android.youtube.player.YouTubeInitializationResult;
+    import com.google.android.youtube.player.YouTubePlayer;
+    import com.google.android.youtube.player.YouTubePlayer.Provider;
+    import com.google.android.youtube.player.YouTubePlayerView;
+
+    import java.util.List;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
+    private YouTubePlayer player;
+    EditText txt;
+    Button but;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = (EditText) findViewById(R.id.text);
+        but = (Button) findViewById(R.id.but);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        this.player = player;
+        if (!wasRestored) {
+            player.cueVideo("fhWaJi1Hsfo"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected Provider getYouTubePlayerProvider() {
+        return youTubeView;
+    }
+
+    public void playme(View view)
+    {
+        try {
+            player.cueVideo(txt.getText().toString());
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, "Wrong Link", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void first(View v)
+    {
+        player.cueVideo("android.resource://"+getPackageName()+"/"+R.raw.v1);
+    }
+    public void second(View v)
+    {
+        player.cueVideo("android.resource://"+getPackageName()+"/"+R.raw.v2);
+    }
+
+}
+
+
